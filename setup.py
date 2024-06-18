2e83ddbb7faf7c86cd29d2b156c4d279c7cfb0c9
swathyashok2610 <swathippriya.a2021@vitstudent.ac.in>
2024-06-18 18:54:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name="Stock Market Prediction by Swathy Priya",
+    name="Stock Market Prediction by Swathy & Arnav",
     version=1.0,
     author="Swathy Priya Ashok",
     packages=find_packages(),
